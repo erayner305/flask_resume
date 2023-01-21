29d5798495bafa6066fa1e603f749598c4663fc3
erayner305 <etr0012@tigermail.auburn.edu>
2023-01-21 14:27:45 -0800

    Updated app.py and removed unnecessary code

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    first_name = "Johns"
-    return render_template('index.html', 
-        f_name = first_name)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    first_name = "Easton"
-    return render_template('about.html', f_name = first_name)
+    return render_template('about.html')
 
 
